cf73800d320dfbeb1292b89a126260717e81ce7d
wing2048 <atraguliauniliantauthr@gmail.com>
2015-11-03 17:36:29 +1100

    added time stopping and randomness and colour tweaks

--- classes.py
+++ classes.py
@@ -1,3 +1,5 @@
+import math
+
 __author__ = 'wing2048'
 import random
 
@@ -8,41 +10,125 @@ enemies = pygame.sprite.Group()
 players = pygame.sprite.Group()
 
 
+def cap(n, c):
+    return c if n >= c else n
+
+
+def limit(n, l, u):
+    return u if n >= u else l if n <= l else n
+
+
+class Game:
+    def __init__(self):
+        self.time_scale = 1
+        self.overlay_size = OVERLAY_SIZE
+        self.overlay = pygame.Surface((self.overlay_size, self.overlay_size))
+        self.overlay_rect = self.overlay.get_rect()
+        self.overlay.set_alpha(1)
+        self.overlay_alpha = 100
+
+    def slow(self, amount):
+        self.time_scale = amount
+
+    def reset_small_overlay(self):
+        self.overlay_size = PLAYER_SIZE
+        self.overlay = pygame.Surface((self.overlay_size, self.overlay_size))
+        self.overlay_rect = self.overlay.get_rect()
+        self.overlay.set_alpha(100)
+        self.overlay_alpha = 100
+
+    def reset_overlay(self):
+        self.overlay_size = OVERLAY_SIZE
+        self.overlay = pygame.Surface((self.overlay_size, self.overlay_size))
+        self.overlay_rect = self.overlay.get_rect()
+        self.overlay.set_alpha(1)
+        self.overlay_alpha = 1
+
+    def update(self):
+        self.overlay = pygame.Surface((self.overlay_rect.width, self.overlay_rect.height))
+        self.overlay.set_alpha(self.overlay_alpha)
+
+
+game = Game()
+
+
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, x, y, s, rc=True):
         self.image = pygame.Surface((s, s))
         self.image.fill((0, 0, 0))
         self.random_colours = rc
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
         pygame.sprite.Sprite.__init__(self)
         enemies.add(self)
 
-        self.increasing_color = 0
+        self.direction = 0
+
+        self.increasing_color = random.randint(0, 2)
         self.color_timer = 0
         self.r = 0
         self.g = 0
         self.b = 0
+        self.x = x
+        self.y = y
+        self.vx = 0
+        self.vy = 0
+
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def move_random(self):
+        self.direction = random.randint(0, 3)
+        if self.direction == 0:
+            self.vy = -random.randint(1, ENEMY_MOVE_AMOUNT)
+        elif self.direction == 1:
+            self.vy = random.randint(1, ENEMY_MOVE_AMOUNT)
+        elif self.direction == 2:
+            self.vx = -random.randint(1, ENEMY_MOVE_AMOUNT)
+        else:
+            self.vx = random.randint(1, ENEMY_MOVE_AMOUNT)
 
     def update(self):
+        self.vx *= 0.98
+        self.vy *= 0.98
         if self.random_colours:
             if self.increasing_color == 0:
-                self.r += ENEMY_COLOUR_CHANGE_SPEED
-                self.b -= ENEMY_COLOUR_CHANGE_SPEED
-                if self.r == 100:
+                self.r += ENEMY_COLOUR_CHANGE_SPEED * game.time_scale
+                self.b -= ENEMY_COLOUR_CHANGE_SPEED * game.time_scale
+                if self.r >= 100:
                     self.increasing_color = 1
             if self.increasing_color == 1:
-                self.g += ENEMY_COLOUR_CHANGE_SPEED
-                self.r -= ENEMY_COLOUR_CHANGE_SPEED
-                if self.g == 100:
+                self.g += ENEMY_COLOUR_CHANGE_SPEED * game.time_scale
+                self.r -= ENEMY_COLOUR_CHANGE_SPEED * game.time_scale
+                if self.g >= 100:
                     self.increasing_color = 2
             if self.increasing_color == 2:
-                self.b += ENEMY_COLOUR_CHANGE_SPEED
-                self.g -= ENEMY_COLOUR_CHANGE_SPEED
-                if self.b == 100:
+                self.b += ENEMY_COLOUR_CHANGE_SPEED * game.time_scale
+                self.g -= ENEMY_COLOUR_CHANGE_SPEED * game.time_scale
+                if self.b >= 100:
                     self.increasing_color = 0
-            self.image.fill((self.r + 150, self.g + 150, self.b + 150))
+            self.image.fill((
+                limit(self.r + 150, 0, 255),
+                limit(self.g + 150, 0, 255),
+                limit(self.b + 150, 0, 255)
+            ))
+        if abs(self.vx) + abs(self.vy) < 1:
+            self.move_random()
+        self.x += self.vx * abs(game.time_scale)
+        self.y += self.vy * abs(game.time_scale)
+        if self.y < 20:
+            self.vy = 2
+        elif self.y > SCREEN_HEIGHT - 20:
+            self.vy = -2
+        elif self.x < 20:
+            self.vx = 2
+        elif self.x > SCREEN_WIDTH - 20:
+            self.vx = -2
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+
+def get_distance(s1, s2):
+    return math.sqrt((s1.x - s2.x) ** 2 + (s1.y - s2.y) ** 2)
 
 
 class Player(pygame.sprite.Sprite):
@@ -53,6 +139,7 @@ class Player(pygame.sprite.Sprite):
             self.key_down = controls['down']
             self.key_left = controls['left']
             self.key_right = controls['right']
+        self.size = s
         self.image = pygame.Surface((s, s))
         self.image.fill((0, 0, 0))
         self.rect = self.image.get_rect()
@@ -64,16 +151,28 @@ class Player(pygame.sprite.Sprite):
         players.add(self)
 
     def update(self, pressed):
+        collide = pygame.sprite.spritecollide(self, enemies, False)
+        for sprite in collide:
+            print('dieded')
+        # flag = False
+        # for enemy in enemies:
+        #     if get_distance(self, enemy) < 50:
+        #         flag = True
+        #         game.slow(0.7)
+        #         break
+        # if not flag:
+        #     game.time_scale = 1
+
         if not self.mouse:
             for ku in self.key_up:
                 if pressed[ku]:
-                    self.vy += -PLAYER_ACCELERATE_Y
+                    self.vy -= PLAYER_ACCELERATE_Y
             for kd in self.key_down:
                 if pressed[kd]:
                     self.vy += PLAYER_ACCELERATE_Y
             for kl in self.key_left:
                 if pressed[kl]:
-                    self.vx += -PLAYER_ACCELERATE_X
+                    self.vx -= PLAYER_ACCELERATE_X
             for kr in self.key_right:
                 if pressed[kr]:
                     self.vx += PLAYER_ACCELERATE_X
--- game.py
+++ game.py
@@ -4,10 +4,11 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SRCALPHA)
 done = False
 r = 0
 g = 0
-b = 0
+b = 200
 increasing_color = 0
-test_enemy = Enemy(10, 10, 10)
-player = Player(50, 50, 10, True, PLAYER_1_CONTROLS)
+for i in range(80):
+    Enemy(400, 300, 10)
+player = Player(50, 50, PLAYER_SIZE, False, PLAYER_1_CONTROLS)
 clock = pygame.time.Clock()
 pygame.mouse.set_visible(False)
 while not done:
@@ -18,26 +19,66 @@ while not done:
             if event.key == pygame.K_ESCAPE:
                 done = True
     if increasing_color == 0:
-        r += COLOUR_CHANGE_SPEED
-        b -= COLOUR_CHANGE_SPEED
-        if r == 100:
+        r += COLOUR_CHANGE_SPEED * round(game.time_scale, 2)
+        b -= COLOUR_CHANGE_SPEED * round(game.time_scale, 2)
+        if r >= COLOUR_BRIGHTNESS:
+            r = COLOUR_BRIGHTNESS
             increasing_color = 1
     if increasing_color == 1:
-        g += COLOUR_CHANGE_SPEED
-        r -= COLOUR_CHANGE_SPEED
-        if g == 100:
+        g += COLOUR_CHANGE_SPEED * round(game.time_scale, 2)
+        r -= COLOUR_CHANGE_SPEED * round(game.time_scale, 2)
+        if g >= COLOUR_BRIGHTNESS:
+            g = COLOUR_BRIGHTNESS
             increasing_color = 2
     if increasing_color == 2:
-        b += COLOUR_CHANGE_SPEED
-        g -= COLOUR_CHANGE_SPEED
-        if b == 100:
+        b += COLOUR_CHANGE_SPEED * round(game.time_scale, 2)
+        g -= COLOUR_CHANGE_SPEED * round(game.time_scale, 2)
+        if b >= COLOUR_BRIGHTNESS:
+            b = COLOUR_BRIGHTNESS
             increasing_color = 0
     pressed = pygame.key.get_pressed()
-    enemies.update()
     players.update(pressed)
+    if pressed[pygame.K_SPACE]:
+        game.time_scale = 0.5
+        game.overlay_rect.width /= 1.2
+        game.overlay_rect.height /= 1.2
+        if game.overlay_alpha < 64:
+            game.overlay_alpha *= 2
+        screen.blit(
+            game.overlay,
+            (
+                player.x + player.size / 2 - game.overlay_rect.width / 2,
+                player.y + player.size / 2 - game.overlay_rect.height / 2
+            )
+        )
+        if game.overlay_rect.width <= PLAYER_SIZE:
+            game.reset_small_overlay()
+    else:
+        if game.overlay_rect.width >= OVERLAY_SIZE * 0.7:
+            game.reset_overlay()
+        else:
+            game.slow(1)
+            game.overlay_rect.width *= 1.2
+            game.overlay_rect.height *= 1.2
+            if game.overlay_alpha > 0:
+                game.overlay_alpha /= 1.2
+            screen.blit(
+                game.overlay,
+                (
+                    player.x + player.size / 2 - game.overlay_rect.width / 2,
+                    player.y + player.size / 2 - game.overlay_rect.height / 2
+                )
+            )
+    enemies.update()
+    game.update()
     trans_overlay = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
-    trans_overlay.set_alpha(100)
-    trans_overlay.fill((r + 150, g + 150, b + 150, 0))
+    trans_overlay.set_alpha(50 * game.time_scale)
+    trans_overlay.fill((
+        limit(r + (250 - COLOUR_BRIGHTNESS) * game.time_scale ** 3, 0, 254),
+        limit(g + (250 - COLOUR_BRIGHTNESS) * game.time_scale ** 3, 0, 254),
+        limit(b + (250 - COLOUR_BRIGHTNESS) * game.time_scale ** 3, 0, 254),
+        0
+    ))
     screen.blit(trans_overlay, (0, 0))
     enemies.draw(screen)
     players.draw(screen)
